6dea423f8ea57a8a46bd20cee1dc96425f038d8e
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2024-03-22 23:04:32 +0530

    adding server folder

--- server/app.js
+++ server/app.js
@@ -5,13 +5,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/budgetgadget', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Serve the React app from the build folder
-app.use(express.static(path.join(__dirname, '../client/build')));
 
-// Handle any other routes by serving the index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 3001;
 
