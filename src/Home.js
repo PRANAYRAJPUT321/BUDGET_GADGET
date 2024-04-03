6ecc99fe3ad97b414fde0eee3ea758f988a1c7ca
PRANAY RAJPUT <pranaydadghaye@gmail.com>
2024-04-03 11:18:24 +0530

    delted some unwanted code

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./Home.css"
 import Navbar from './Navbar';
+import { useLocation } from 'react-router-dom';
 function Home(){
+
+  const location=useLocation()
   return(
 <div>
   <Navbar/>
-  <h1>this is home page</h1>
+  <h1>Hello {location.state.id}Welcome to home page
+  </h1>
 
 </div>
   );
